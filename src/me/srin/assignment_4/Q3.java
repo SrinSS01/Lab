0014579c0d12edbfe5547ee61845cac2609a3f31
Srin <60167747+SrinSS01@users.noreply.github.com>
2022-04-14 11:11:02 +0530

    no commit message

--- src/me/srin/assignment_4/Q3.java
+++ src/me/srin/assignment_4/Q3.java
@@ -1,14 +1,14 @@
 package me.srin.assignment_4;
 
+import java.util.Arrays;
+
 /**
  * Write a java program to split the given string into words:
  * <div style="font-weight: bold;">“Hello, How are you.”</div>*/
 public class Q3 {
     public static void main(String[] args) {
         String str = "Hello, How are you.";
-        String[] words = str.split(" ");
-        for (String word : words) {
-            System.out.println(word);
-        }
+        String[] words = str.split("[, .]+");
+        System.out.println(Arrays.toString(words));
     }
 }
